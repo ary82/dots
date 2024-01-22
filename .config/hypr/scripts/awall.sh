9ed10de90b329a016afad53c807f101333775216
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-01-22 18:10:39 +0530

    update

--- .config/hypr/scripts/awall.sh
+++ .config/hypr/scripts/awall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wall=$(find ~/Pictures/Wallpapers/Anime -maxdepth 1 -type f | shuf -n 1)
+wall=$(find ~/Pictures/Wallpapers/Anime -type f | shuf -n 1)
 swww img $wall --transition-type wipe --transition-step 5 --transition-fps 60
