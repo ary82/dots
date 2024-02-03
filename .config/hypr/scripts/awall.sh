961ed313601c151365b16bebc11688abdb7b0d67
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-02-03 18:58:05 +0530

    add renders

--- .config/hypr/scripts/awall.sh
+++ .config/hypr/scripts/awall.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 wall=$(find ~/Pictures/Wallpapers/Anime -maxdepth 1 -type f | shuf -n 1)
 swww img $wall --transition-type wipe --transition-fps 60
+
+CHECK=$(cat ~/.zshrc | grep "Anime_renders")
+if ! [[ $CHECK ]]; then
+	echo "find /mnt/veracrypt1/0Wallpapers/Anime_renders/ -maxdepth 1 -type f | shuf -n 1 | xargs chafa --align center" >>~/.zshrc
+fi
--- .config/hypr/scripts/wall.sh
+++ .config/hypr/scripts/wall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wall=$(find ~/Pictures/Wallpapers -maxdepth 1 -type f | shuf -n 1)
 swww img $wall --transition-type wipe --transition-fps 60
+sed -i '/Anime_renders/d' ~/.zshrc
