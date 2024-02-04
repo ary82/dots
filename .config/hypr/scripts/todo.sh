5877bef5c227f6e9ea660620a8b07b41c3d5f00e
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-02-04 21:27:48 +0530

    update

--- .config/hypr/scripts/todo.sh
+++ .config/hypr/scripts/todo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-COUNT=$(wc -l <~/todo.txt)
+COUNT=$(($(wc -l <~/todo.txt)-1))
 TODOS=$(cat ~/todo.txt | head -c -1 - | sed -z 's/\n/\\n/g')
 printf '{"text": "%s", "tooltip": "%s"}\n' "$COUNT" "$TODOS"
--- .config/hypr/scripts/wall.sh
+++ .config/hypr/scripts/wall.sh
@@ -2,3 +2,5 @@
 wall=$(find ~/Pictures/Wallpapers -maxdepth 1 -type f | shuf -n 1)
 swww img $wall --transition-type wipe --transition-fps 60
 sed -i '/Anime_renders/d' ~/.zshrc
+cat .cache/swww/eDP-1 | xargs -I {} cp {} ~/.config/rofi/images/img
+convert ~/.config/rofi/images/oimg -resize 40% ~/.config/rofi/images/img
