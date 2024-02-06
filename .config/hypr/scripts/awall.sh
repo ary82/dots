74bbbc1f93a7aaa4b80dfddf3cb0d3336f5900cc
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-02-06 14:34:14 +0530

    update lenopow, awall scripts

--- .config/hypr/scripts/awall.sh
+++ .config/hypr/scripts/awall.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 wall=$(find ~/Pictures/Wallpapers/Anime -maxdepth 1 -type f | shuf -n 1)
 swww img $wall --transition-type wipe --transition-fps 60
-cat .cache/swww/eDP-1 | xargs -I {} cp {} ~/.config/rofi/images/oimg
+cat ~/.cache/swww/eDP-1 | xargs -I {} cp {} ~/.config/rofi/images/oimg
 convert ~/.config/rofi/images/oimg -resize 40% ~/.config/rofi/images/img
 
 CHECK=$(cat ~/.zshrc | grep "Anime_renders")
 if ! [[ $CHECK ]]; then
 	echo "find /mnt/veracrypt1/0Wallpapers/Anime_renders/ -maxdepth 1 -type f | shuf -n 1 | xargs chafa --align center" >>~/.zshrc
+fi
+
+# Move the selected image to old folder
+cat ~/.cache/swww/eDP-1 | xargs -I {} mv {} ~/Pictures/Wallpapers/Anime/old/
+
+# Move the old images back to the main folder
+OLD_FILES=$(find ~/Pictures/Wallpapers/Anime/old -maxdepth 1 -type f | wc -l)
+if [[ $OLD_FILES -gt 50 ]]; then
+	mv ~/Pictures/Wallpapers/Anime/old/* ~/Pictures/Wallpapers/Anime/
 fi
--- .config/hypr/scripts/lenopow.sh
+++ .config/hypr/scripts/lenopow.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
 CHECK=$(lenopow -s | cut --delimiter=" " --fields=3)
-if [ "$CHECK" == "ENABLED" ]; then
-	sudo lenopow -d
-	notify-send --urgency=low "Battery Protection Disabled"
-else
-	sudo lenopow -e
-	notify-send --urgency=low "Battery Protection Enabled"
-fi
+
+protection_toggle() {
+	if [ "$CHECK" == "ENABLED" ]; then
+		sudo lenopow -d
+		notify-send --urgency=low "Lenopow Disabled"
+	else
+		sudo lenopow -e
+		notify-send --urgency=low "Lenopow Enabled"
+	fi
+}
+protection_status() {
+	notify-send --urgency=low "Lenopow status: $CHECK"
+}
+
+case "$1" in
+"--toggle")
+	protection_toggle
+	;;
+*)
+	protection_status
+	;;
+esac
