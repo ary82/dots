f4cb9d93767d62e0cae5189bfcaf8a4f8304d096
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-02-17 14:44:55 +0530

    update

--- .config/hypr/scripts/todo.sh
+++ .config/hypr/scripts/todo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-COUNT=$(($(wc -l <~/todo.txt)-1))
-TODOS=$(cat ~/todo.txt | head -c -1 - | sed -z 's/\n/\\n/g')
+INPUT="/home/ary/todo.txt"
+COUNT=$(($(wc -l <"$INPUT") - 1))
+TODOS=$(cat $INPUT | head -c -1 - | sed -z 's/\n/\\n/g')
 printf '{"text": "%s", "tooltip": "%s"}\n' "$COUNT" "$TODOS"
